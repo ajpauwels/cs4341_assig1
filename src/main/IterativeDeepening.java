f965fb5967ca207c76abaf0fe63322266eaff274
Alexandre Pauwels <ajpauwels@wpi.edu>
2016-09-06 19:11:09 -0400

    Wrote the single-node loop for IterativeDeepening

--- src/main/IterativeDeepening.java
+++ src/main/IterativeDeepening.java
@@ -1,8 +1,34 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class IterativeDeepening {
-
-	public IterativeDeepening() {
+	private int level;
+	private Configuration setup;
+	
+	public IterativeDeepening(Configuration config) {
+		this.setup = config;
 	}
-
+	
+	public boolean run() {
+		Date start = new Date();
+		level = 0;
+		
+		return false;
+	}
+	
+	private Operation loop(int val) {
+		ArrayList<Operation> ops = setup.getOperations();
+		
+		for (Operation op : ops) {
+			int result = op.execute(val);
+			
+			if (result == setup.getEndVal()) {
+				return op;
+			}
+		}
+		
+		return null;
+	}
 }
