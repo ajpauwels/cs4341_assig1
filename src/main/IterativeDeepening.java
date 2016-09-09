99292f43f85b055da8ca2ba2c4ca2d06527364e1
howhelan <howhelan@wpi.edu>
2016-09-09 13:26:59 -0400

    Fixed timing issues with iterative and finalized outpu

--- src/main/IterativeDeepening.java
+++ src/main/IterativeDeepening.java
@@ -18,6 +18,9 @@ public class IterativeDeepening {
 	private int numberOfNodes = 0;
 	private int maxDepth = 0;
 	
+	Date start;
+	Date end;
+	
 	public IterativeDeepening(Configuration config) {
 		this.setup = config;
 		currentPath = new ArrayList<Operation>();
@@ -29,19 +32,24 @@ public class IterativeDeepening {
 	 */
 	
 	public boolean run() {
-		Date start = new Date();
-		Date end = new Date();
 		iterativeDepth = 1;
 		
+		start = new Date();
+		end = new Date();
+		
 		//while time has not run out and result hasn't been found
-		while(end.getTime() - start.getTime() < setup.getTimeLimit() && !resultFound){
+		while(end.getTime() - start.getTime() < setup.getTimeLimit() * 1000 && !resultFound){
 			
 			if( loop(setup.getBeginVal(), 0) == setup.getEndVal() ){ //run the recursive loop
 					resultFound = true;
 			}
 		
 			iterativeDepth++;
-			end = new Date();
+		}
+		
+		System.out.println(end.getTime() - start.getTime());
+		if(end.getTime() - start.getTime() > setup.getTimeLimit() * 1000){
+			System.out.println("RAN OUT OF TIME");
 		}
 
 		//print out the results
@@ -55,9 +63,6 @@ public class IterativeDeepening {
 		System.out.println("Search required: " + (((end).getTime() - start.getTime()) / 1000.0 ));
 		System.out.println("Nodes expanded: " + numberOfNodes);
 		System.out.println("Maximum search depth: " + (maxDepth+1));
-		
-		
-		
 		
 		return false;
 	}
@@ -84,10 +89,13 @@ public class IterativeDeepening {
 				runningBestPath = (ArrayList<Operation>) currentPath.clone();
 			}
 			
+
+			end = new Date();
+			
 			if (result == setup.getEndVal()) {
 				resultFound = true;
 				return result;
-			}else if(currentDepth < iterativeDepth){ //if not at result, and not past max iterative depth, recurse into next node
+			}else if(currentDepth < iterativeDepth && ((end.getTime() - start.getTime() < setup.getTimeLimit()*1000))){ //if not at result, and not past max iterative depth, recurse into next node
 				if ( currentDepth > maxDepth) maxDepth = currentDepth;
 				loop(result, ++currentDepth);
 			}
